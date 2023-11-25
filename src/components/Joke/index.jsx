0217a8fe3e18bd8d524c2e9bcf926904c1ff2599
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-11-25 17:39:01 +0100

    question15

---
+++ src/components/Joke/index.jsx
@@ -0,0 +1,22 @@
+import "./styly.css";
+import { useState } from "react";
+export const Joke = ({
+  userAvatar,
+  userName,
+  text,
+  likes,
+  dislikes,
+  onUserName,
+}) => {
+//   const [likeCount, setLikeCount] = useState("0");
+//   const [dislikeCount, setDislikeCount] = useState("0");
+//   const [userAvatar, setUserAvatar] = useState(null);
+//   const [userName, setUserName] = useState("John");
+//   const [text, SetText] = useState("Dobrý den");
+//   const address = "http://localhost:4000/api/jokes";
+  return (
+    <>
+     
+    </>
+  );
+};
