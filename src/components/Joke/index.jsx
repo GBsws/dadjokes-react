2e47067015ee40f20c1e30b1d2fa74235b2b147a
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-11-25 19:30:31 +0100

    question16

--- src/components/Joke/index.jsx
+++ src/components/Joke/index.jsx
@@ -1,22 +1,56 @@
 import "./styly.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 export const Joke = ({
   userAvatar,
   userName,
   text,
   likes,
-  dislikes,
-  onUserName,
+  dislikes
 }) => {
-//   const [likeCount, setLikeCount] = useState("0");
-//   const [dislikeCount, setDislikeCount] = useState("0");
-//   const [userAvatar, setUserAvatar] = useState(null);
-//   const [userName, setUserName] = useState("John");
-//   const [text, SetText] = useState("Dobrý den");
-//   const address = "http://localhost:4000/api/jokes";
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
+  useEffect(()=>{
+    const fetchJoke=async ()=>{
+    const response= await fetch(' https://raw.githubusercontent.com/Czechitas-podklady-WEB/daweb-test/deploy/jokes.json');
+    const data = await response.json()
+    }
+    fetchJoke()
+  },[])
+
   return (
     <>
-     
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src={userAvatar}
+            />
+            <p className="user-name">{userName}</p>
+          </div>
+          <p className="joke-text">
+           {text}
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            className="btn-like btn-like--up"
+            onClick={() => setLikeCount(likeCount + 1)}
+            id="btn-up"
+          >{likes}</button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likeCount}
+          </span>
+          <button
+            className="btn-like btn-like--down"
+            onClick={() => setDislikeCount(dislikeCount + 1)}
+            id="btn-down"
+          >{dislikes}</button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {dislikeCount}
+          </span>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,56 +1,16 @@
 import './style.css';
-//import {Joke} from '../../components/Joke'
-import{useState} from 'react'
+import {Joke} from '../../components/Joke'
+import {useState} from 'react'
 
 export const HomePage = () => {
-  //const address = "http://localhost:4000/api/jokes";
-  const [likeCount, setLikeCount] = useState(0);
-  const [dislikeCount, setDislikeCount] = useState(0);
-  
+  const [jokes, setJokes] = useState(null);
   return (
-    <div className="container">
-      <header>
-        <h1>React webová aplikace</h1>
-      </header>
-     <main>
+    <main>
       <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke-text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button onClick={() => setLikeCount(likeCount + 1)}
-            
-            id="btn-up"
-            className="btn-like btn-like--up"
-          ></button>
-          <span  id="likes-up" className="likes-count likes-count--up">
-            {likeCount}
-          </span>
-          <button
-            onClick={() => setDislikeCount(dislikeCount + 1)}
-            id="btn-down"
-            className="btn-like btn-like--down"
-          ></button>
-          <span  id="likes-down" className="likes-count likes-count--down">
-            {dislikeCount}
-          </span>
-        </div>
-      </div>
+      {jokes.map((joke)=>(
+      <Joke key={joke.id} userName={joke.userName} userAvatar={joke.userAvatar} text={joke.text} likes={joke.likes} dislikes={joke.dislikes} />
+      ))}
       </div>
       </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-      </div>
-  );
-};
+      )
+      }
